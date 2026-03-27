069c786a8215ae2e90ee2c50b3d8084077ce2be5
lfaber2006 <45172930+lfaber2006@users.noreply.github.com>
2026-03-27 11:40:32 -0400

    Create MainActivity.kt

---
+++ app/app/src/main/java/com/google/android/apps/photos/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.google.android.apps.photos
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Force a redirect to the Immich app
+        val launchIntent = packageManager.getLaunchIntentForPackage("app.immich.app")
+        
+        if (launchIntent != null) {
+            startActivity(launchIntent)
+        }
+        
+        finish() // Close the redirector immediately
+    }
+}
